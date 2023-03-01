ccb2a65990fc16a51c3638cc79a4aa097fdc448e
Owenzo7 <owenrogers545@gmail.com>
2023-03-01 20:22:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((item) =>
+        this.setState(
+          () => {
+            return { items: item };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Find items"
+          onChange={(event) => {
+            console.log(event.target.value);
+
+            const searchitems = event.target.value.toLocaleLowerCase();
+
+            const filteredItems = this.state.items.map((item) => {
+              return item.title.toLocaleLowerCase().includes(searchitems);
+            });
+
+            this.setState(() => {
+              return {items: filteredItems};
+            });
+          }}
+        />
+        {this.state.items.map((item) => {
+          return (
+            <div key={item.id}>
+              <h1>{item.title}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-export default App;
