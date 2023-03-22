6b07f4dd2a2e49019bd5535b06c03d81f599e6fe
Owenzo7 <owenlee545@gmail.com>
2023-03-22 20:41:13 +0300

    done with most of the UI

--- src/Components/Itemlists/Itemlists.component.jsx
+++ src/Components/Itemlists/Itemlists.component.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import "./Item-list.styles.css";
 
 export default class Itemlists extends Component {
   render() {
     const { Items } = this.props;
     return (
-      <div>
-        {Items.map((item) => (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-          </div>
-        ))}
+      <div className="card-list">
+        {Items.map((item) => {
+          const { id, description, image, title } = item;
+          return (
+            <div className="card-container" key={id}>
+              <img alt={`item${title}`} src={image} width={180} height={180} />
+              <h2>{title}</h2>
+              <p>{description}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
