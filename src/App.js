a3efe14a19c19c16ef76cf45f4c08a28e0dbc7c7
Owenzo7 <owenlee545@gmail.com>
2023-03-24 16:35:04 +0300

    deal with the issue using the useEffect hook

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Itemlists from "./Components/Itemlists/Itemlists.component";
 import Searchbox from "./Components/SearchBox/Searchbox.component";
 
 const App = () => {
-  const [searchField, setsearchField] = useState("")
+  const [searchField, setsearchField] = useState("");
+  const [items, setitems] = useState([]);
 
-  console.log({searchField})
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((users) => setitems(users));
+
+  console.log({ searchField });
 
   const onSearchChange = (event) => {
     console.log(event.target.value);
@@ -14,18 +19,23 @@ const App = () => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
 
     setsearchField(searchFieldString);
-
-    
   };
 
+  const filteredItems = items.filter((item) => {
+    return item.title.toLocaleLowerCase().includes(searchField);
+  });
 
   return (
     <div className="App">
       <h1 className="app-title">Rogers Eccomerce Store</h1>
 
-      <Searchbox onChangeHandler={onSearchChange} placeholder="search Items" className="items-search-box" />
+      <Searchbox
+        onChangeHandler={onSearchChange}
+        placeholder="search Items"
+        className="items-search-box"
+      />
 
-      {/* <Itemlists Items={filteredItems} /> */}
+      <Itemlists Items={filteredItems} />
     </div>
   );
 };
@@ -38,21 +48,6 @@ const App = () => {
 //       items: [],
 //       searchField: "",
 //     };
-//   }
-
-//   componentDidMount() {
-//     fetch("https://fakestoreapi.com/products")
-//       .then((response) => response.json())
-//       .then((item) =>
-//         this.setState(
-//           () => {
-//             return { items: item };
-//           },
-//           () => {
-//             console.log(this.state);
-//           }
-//         )
-//       );
 //   }
 
 //   onSearchChange = (event) => {
